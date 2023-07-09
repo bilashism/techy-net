863a329492d5953bd1a7510aefa4eb217f660422
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2023-07-09 11:18:07 +0600

    added store and addToCart

---
+++ src/redux/features/cart/cartSlice.ts
@@ -0,0 +1,28 @@
+import { IProduct } from '@/types/globalTypes';
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+
+interface ICart {
+  products: IProduct[];
+}
+const initialState: ICart = {
+  products: [],
+};
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<IProduct>) => {
+      const existingProduct = state.products.find(
+        (product) => product._id === action.payload._id
+      );
+      if (existingProduct) {
+        existingProduct.quantity = existingProduct.quantity! + 1;
+      } else {
+        state.products.push(action.payload);
+      }
+    },
+  },
+});
+export const { addToCart } = cartSlice.actions;
+export const cartReducer = cartSlice.reducer;
